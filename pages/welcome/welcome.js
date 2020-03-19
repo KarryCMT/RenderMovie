8c27ec8b10b10e56778457f282d2ad98551f1602
KarryCMT <1570606465@qq.com>
2020-03-19 17:52:38 +0800

    '提交'

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,66 +1,25 @@
 // pages/welcome/welcome.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  onTap(){
+    // wx.navigateTo({
+    //   url: '../posts/posts',
+    // })
+    wx:wx.redirectTo({
+      url: '../posts/posts',
+    })
+    // console.log('onTap');
   },
+  //事件冒泡 catchtap可以阻止冒泡
+  // onContainerTap:function(){
+  //   console.log('Container Tap')
+  // },
+  // onSubTap:function(){
+  //   console.log('Sub Tap')
+  // },
+  // onLoad(){
+  
+  // },
+  // onUnload:function(){
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  // }
 })
